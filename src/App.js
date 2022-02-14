c8c80310f497566060992334e94da2cd8ff6e009
Ivo Cardozo <ivo.cardozo@ip-192-168-1-2.us-west-2.compute.internal>
2022-02-14 18:21:00 -0400

    working with Refs & Forward Refs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Fragment, useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  const[cartIsShown, setCartIsShown] = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
@@ -15,13 +17,13 @@ function App() {
   };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
---
+++ src/store/cart-context.js
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react';
+
+const Cartcontext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+export default Cartcontext;
