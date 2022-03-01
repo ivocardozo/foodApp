adea76aeb825ad07c41c0816bc3c84896d4930ae
Ivo Cardozo <ivo.cardozo@ascbominimac54.local>
2022-03-01 13:36:07 -0400

    adding cart reducer and refs

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,14 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input key={props.id} {...props.input}/>
+      <input ref={ref} key={props.id} {...props.input}/>
     </div>
   );
-}
+});
 
 export default Input;
