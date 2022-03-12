26f41bca31d3e66f6f8eb320cc024a46d3ff31d9
Ivo Cardozo <ivo.cardozo@ascbominimac54.local>
2022-03-12 15:34:32 -0400

    adding event to close an open cart

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import ReactDOM from "react-dom";
 import classes from './Modal.module.css';
 
-const Backdrop = () => <div className={classes.backdrop}/>;
+const Backdrop = (props) => <div className={classes.backdrop} onClick={props.onClose}/>;
 
 const ModalOverlay = props => {
   return(
@@ -15,7 +15,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = props => {
   return(
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
       {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </Fragment>
   )
