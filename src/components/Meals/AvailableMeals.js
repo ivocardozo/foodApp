fc261a3ad6c84c5658a1a870bab30966ec50ff5d
icarvar <ivo.cardozo@preservica.com>
2022-11-28 12:26:43 -0400

    adding meal items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -28,7 +29,15 @@ const DUMMY_MEALS = [
     },
   ]
   const AvailableMeals = props => {
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+    const mealList = DUMMY_MEALS.map(meal => 
+        <MealItem 
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+        )
     return (
         <section className={classes.meals}>
             <Card>
