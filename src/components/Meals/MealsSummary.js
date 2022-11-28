015d701da3ccf6c4ad020294aa34625c81120202
icarvar <ivo.cardozo@preservica.com>
2022-11-28 11:33:11 -0400

    adding meals

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from './MealsSummary.js';
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious Food, Delivered to you</h2>
+            <p>
+                Choose your favorite meal from our best selection of available meals
+                and enjoy a dlicious lunch or dinner at home.
+            </p>
+            <p>
+                All ours meals are cooked with high-quality ingredients, just-in-time and
+                ofu course by experienced chefs!
+            </p>
+        </section>
+    )
+}
+
+export default MealsSummary;
