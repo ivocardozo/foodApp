9e094a5cc4fadb609bbe3a189c9f45a5a36e1201
icarvar <ivo.cardozo@preservica.com>
2023-01-16 16:08:31 -0400

    Adding Better User Feedback

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -52,6 +52,10 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     }
   }
+
+  if(action.type === 'CLEAR') {
+    return defaultCartState
+  }
   return defaultCartState
 }
 const CartProvider = props => {
@@ -63,12 +67,16 @@ const CartProvider = props => {
   const removeItemFromCartHandler = (id) => {
     dispatchCartAction({type: 'REMOVE', id: id})
   };
+  const clearCartHandler = () => {
+    dispatchCartAction({type: 'CLEAR'})
+  }
 
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
-    removeItem: removeItemFromCartHandler
+    removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return(<CartContext.Provider value={cartContext}>
