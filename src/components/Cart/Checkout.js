fc307a587ff270a65fa8823ba14a8a37eca4e702
icarvar <ivo.cardozo@preservica.com>
2023-01-16 21:59:00 -0400

    Submitting & Sending Cart Data

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -46,6 +46,13 @@ function Checkout(props) {
         if (!formIsValid) {
             return
         }
+
+        props.onConfirm({
+            name: enteredName,
+            street: enteredStreet,
+            city: enteredCity,
+            postalCode: enteredPostalCode
+        })
     }
 
     const nameControlClasses = `${classes.control} ${
