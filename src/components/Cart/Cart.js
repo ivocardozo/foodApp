ecdaaeac7cbb015cf04d9a768685f7c191a0ca56
icarvar <ivo.cardozo@preservica.com>
2023-01-17 19:49:50 -0400

    Submitting & Sending Cart Data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -24,6 +24,16 @@ const Cart = props => {
     cartCtx.addItem({...item, amount: 1})
   };
 
+  const submitOrderHandler = userData => {
+    fetch('https://react-http-282b2-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST', 
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    })
+  }
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -52,7 +62,7 @@ const Cart = props => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose}/>}
+      {isCheckout && <Checkout onCancel={props.onClose} onConfirm={submitOrderHandler}/>}
       {!isCheckout && modalActions}
     </Modal>
   );
