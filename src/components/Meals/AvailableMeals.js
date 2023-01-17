2c0f90de5893da18cb6bba1f555e7e711b73da90
icarvar <ivo.cardozo@preservica.com>
2023-01-17 09:01:16 -0400

    Handling the loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = props => {
   const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchMeals = async() => {
@@ -23,10 +24,19 @@ const AvailableMeals = props => {
         })
       }
       setMeals(loadedMeals)
+      setIsLoading(false)
     }
 
     fetchMeals()
   }, [])
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
   const mealsList = meals.map(meal => 
     <MealItem 
       key={meal.id}
