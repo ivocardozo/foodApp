f8d16fe5e8e2aaf4bfbd9ef8145dd1fe9cf848df
icarvar <ivo.cardozo@preservica.com>
2023-01-17 19:22:42 -0400

    Handling http errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,10 +7,16 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = props => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState(null)
 
   useEffect(() => {
     const fetchMeals = async() => {
       const response = await fetch('https://react-http-282b2-default-rtdb.firebaseio.com/meals.json')
+
+      if(!response.ok) {
+        throw new Error('Something went wrong')
+      }
+  
       const responseData = await response.json()
 
       const loadedMeals = []
@@ -27,7 +33,10 @@ const AvailableMeals = props => {
       setMeals(loadedMeals)
       setIsLoading(false)
     }
-    fetchMeals()
+    fetchMeals().catch(error => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   }, [])
 
   if (isLoading) {
@@ -46,6 +55,14 @@ const AvailableMeals = props => {
       price={meal.price}
     />
   )
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
   return (
     <section className={classes.meals}>
       <Card>
